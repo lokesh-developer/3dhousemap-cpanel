db19b6f3a3f10117bfc54ab3ade3c484303d358b
Diligent Coder <kabral1999@gmail.com>
2022-11-01 19:27:13 +0530

    :)

--- js/external.js
+++ js/external.js
@@ -1,7 +1,2 @@
-$(document).ready(function(){
-        $(".toggle-btn").click(function(){
-            $('#sidebar').toggleClass('active');
-            $('.sidebar-menu li span').toggleClass('menu-hide');
-});
-});
+
 
