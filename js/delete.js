f7027e6bf5cf2930c7c7917f8fc8b049693b8bd9
ajinkyaKahane <kahaneajinkya051@gmail.com>
2022-11-05 22:11:23 +0530

    Delete Request Completed

--- js/delete.js
+++ js/delete.js
@@ -1,6 +1,7 @@
 import firebase from './firebase.js'
+import { collection, doc, getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
 
-const { app, db} = firebase
+// const { app, db} = firebase
 export async function fetchB(){
     const btn = document.getElementsByClassName('removeButton');
     console.log(btn)
@@ -9,26 +10,16 @@ export async function fetchB(){
     for(let i=0; i<b.length; i++){
         btn[i].addEventListener('click', async function(){
             console.log(btn[i].id)
-
+            
             let ans = confirm('Do You Really Want To Reject Request?')
             if(ans){
-                console.log('Deleting', btn[i].id)
-                db.collection('joinedus').doc(btn[i].id.toString()).delete().then(()=>{
-                    console.log('Deleted Successfully')
-                }).catch(error => console.log(error))
-                // d.then(()=>console.log('Deleted', btn[i].id))
-
-                const userRef = doc(firebase.db, 'joinedus', btn[i].id)
-
-                // deleteDoc(userRef).then(()=>{console.log('Deleted', btn[i].id); window.location.reload()})
-
+                const del = doc(firebase.db, 'joinedus', `${btn[i].id}`)
+                deleteDoc(del).then(()=>window.location.reload())
             } else {
                 alert('Request Not Deleted!')
             }
         })
     }
-    
-
 }
 // function removeRequest(){
 //     const id = btn.id;
