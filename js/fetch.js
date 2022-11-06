ad9dff1f775b2e263ab6c26da4062167eb7ce8df
ajinkyaKahane <kahaneajinkya051@gmail.com>
2022-11-06 15:26:26 +0530

    Income Section Completed.

--- js/fetch.js
+++ js/fetch.js
@@ -40,8 +40,8 @@ export async function setCreators() {
               <td>${data.email}</td>
               <td>${data.contact}</td>
               <td>
-              <button id='${doc.id}' style="cursor: pointer;" onclick="window.location = '/creators.html'">Remove</button>
-                        <button  style="cursor: pointer;" onclick="window.location = '/creators.html'">View</button>
+              <button id='${doc.id}' class='removeButton' style="cursor: pointer;">Remove</button>
+              <button id='${doc.id}' class='viewProfile' style="cursor: pointer;">View</button>
                 </td>
             `;
         creatorsTable.appendChild(tr);
@@ -72,6 +72,44 @@ export async function setAllCreators() {
     });
 }
 
-// export async function removeRequest(id){
-//     console.log('Deletion Initiated')
-// }
+export async function fetchIncome() {
+    const creators = collection(firebase.db, 'joinedus');
+    const docs = await getDocs(creators);
+    const incomeTable = document.getElementById('income-table');
+
+    docs.docs.forEach((doc) => {
+        const data = doc.data();
+
+        const name = data.fullName;
+        let totalIncome = 0;
+        data.plansSold.map((e) => totalIncome += parseFloat(e.price))
+        console.log(totalIncome)
+        const tr = document.createElement("tr");
+
+        let oneMonthIncome = 0;
+        var seconds = new Date().getTime() / 1000;
+        const todayTS = seconds * 1000;
+        const past30thTS = (seconds  -2592000)*1000;
+
+        data.plansSold.map((e) => {
+                console.log(e.timestamp.seconds * 1000) //Timestamp Of The Date When The Product Was Purchased
+                console.log(seconds * 1000) //Today's Timestamp
+                console.log((seconds  -2592000)*1000) //Timestamp Of Past 30th Day From Today
+                
+                const purchaseTS = e.timestamp.seconds * 1000; 
+
+                if(todayTS - purchaseTS <  todayTS - past30thTS){
+                    oneMonthIncome+=parseFloat(e.price)
+                }
+        })
+        tr.innerHTML = `
+              <td><a href="./creator.html?id=${doc.id}">${data.fullName}</a></td>
+              <td>${data.email}</td>
+              <td>${oneMonthIncome}</td>
+              <td>${totalIncome}</td>
+              <td>
+              </td>
+            `;
+        incomeTable.appendChild(tr);
+    })
+}
