4a2b23c53cb379fa1a3707629c265d77c8edd8e2
Diligent Coder <kabral1999@gmail.com>
2022-12-30 16:06:11 +0530

    :)

--- js/delete.js
+++ js/delete.js
@@ -1,56 +1,84 @@
-import firebase from './firebase.js'
-import { collection, doc, getDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
-import { ref, deleteObject } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
+import firebase from './firebase.js';
+import {
+  collection,
+  doc,
+  getDoc,
+  deleteDoc
+} from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
+import { ref, deleteObject } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js';
 
 export async function deleteRequest() {
-    const btn = document.getElementsByClassName('removeButton');
+  const btn = document.getElementsByClassName('removeButton');
 
-    var b = Array.from(btn);
+  var b = Array.from(btn);
 
-    for (let i = 0; i < b.length; i++) {
+  for (let i = 0; i < b.length; i++) {
+    btn[i].addEventListener('click', async function () {
+      let ans = confirm('Do You Really Want To Reject Request?');
 
-        btn[i].addEventListener('click', async function () {
+      if (ans) {
+        const del = doc(firebase.db, 'joinedus', `${btn[i].id}`);
+        const doc1 = await getDoc(del);
 
-            let ans = confirm('Do You Really Want To Reject Request?')
+        const sDRef = ref(firebase.storage, `${doc1.data().sampleDesign}`);
+        const eQRef = ref(firebase.storage, `${doc1.data().educationQualification}`);
 
-            if (ans) {
-                const del = doc(firebase.db, 'joinedus', `${btn[i].id}`)
-                const doc1 = await getDoc(del)
-
-                const sDRef = ref(firebase.storage, `${doc1.data().sampleDesign}`)
-                const eQRef = ref(firebase.storage, `${doc1.data().educationQualification}`)
-
-                deleteObject(sDRef).then(() => deleteObject(eQRef).then(() => console.log('Deletion Completed')).cathc(() => console.log('Some Error Occured While Deleting Educational Qualifications'))).catch(() => console.log('Some Error Occured While Deleting Sample Design'))
-                deleteDoc(del).then(() => console.log('Request Rejected')).catch(() => console.log('Some Error Occured While Rejecting Request'))
-            } else {
-                alert('Request Not Deleted!')
-            }
-        })
-    }
+        deleteObject(sDRef)
+          .then(() =>
+            deleteObject(eQRef)
+              .then(() => console.log('Deletion Completed'))
+              .cath(() =>
+                console.log('Some Error Occured While Deleting Educational Qualifications')
+              )
+          )
+          .catch(() => console.log('Some Error Occured While Deleting Sample Design'));
+        deleteDoc(del)
+          .then(() => {
+            console.log('Request Rejected');
+            window.location.reload();
+          })
+          .catch(() => console.log('Some Error Occured While Rejecting Request'));
+      } else {
+        alert('Request Not Deleted!');
+      }
+    });
+  }
 }
 
 export async function deleteProduct() {
+  const btn = document.getElementsByClassName('removeProduct');
+  var b = Array.from(btn);
 
-    const btn = document.getElementsByClassName('removeProduct');
-    var b = Array.from(btn);
+  for (let i = 0; i < b.length; i++) {
+    btn[i].addEventListener('click', async function () {
+      let ans = confirm('Do You Really Want To Delete Product?');
 
-    for (let i = 0; i < b.length; i++) {
-        btn[i].addEventListener('click', async function () {
+      if (ans) {
+        const del = doc(firebase.db, 'products', `${btn[i].id}`);
+        const doc1 = await getDoc(del);
 
-            let ans = confirm('Do You Really Want To Delete Product?')
+        const imRef = ref(firebase.storage, `${doc1.data().image}`);
+        const mFRef = ref(firebase.storage, `${doc1.data().mainFile}`);
 
-            if (ans) {
-                const del = doc(firebase.db, 'products', `${btn[i].id}`)
-                const doc1 = await getDoc(del);
-
-                const imRef = ref(firebase.storage, `${doc1.data().image}`)
-                const mFRef = ref(firebase.storage, `${doc1.data().mainFile}`)
-
-                deleteObject(imRef).then(() => deleteObject(mFRef).then(() => console.log('Deletion Completed')).cathc(() => console.log('Some Error Occured While Deleting Main File'))).catch(() => console.log('Some Error Occured While Deleting Image'))
-                deleteDoc(del).then(() => console.log('Request Rejected')).catch(() => console.log('Some Error Occured While Rejecting Request'))
-            } else {
-                alert('Request Not Deleted!')
-            }
-        })
-    }
+        deleteObject(imRef)
+          .then(() =>
+            deleteObject(mFRef)
+              .then(() => {
+                console.log('Deletion Completed');
+                window.location.reload();
+              })
+              .cathc(() => {
+                console.log('Some Error Occured While Deleting Main File');
+                alert('Some Error Occured While Deleting Main File');
+              })
+          )
+          .catch(() => console.log('Some Error Occured While Deleting Image'));
+        deleteDoc(del)
+          .then(() => console.log('Request Rejected'))
+          .catch(() => console.log('Some Error Occured While Rejecting Request'));
+      } else {
+        alert('Request Not Deleted!');
+      }
+    });
+  }
 }
--- js/fetch.js
+++ js/fetch.js
@@ -1,19 +1,22 @@
-import firebase from "./firebase.js";
-import { collection, doc, getDocs } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
-
+import firebase from './firebase.js';
+import {
+  collection,
+  doc,
+  getDocs
+} from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
 
 export async function setProducts() {
-    const products = collection(firebase.db, "products");
-    const docSnap = await getDocs(products);
-    // ////console.log(docSnap.docs);
-    const table = document.getElementById("uploads-table");
-    // ////console.log(creatorsTable);
-    docSnap.docs.reverse().forEach((doc) => {
-        ////console.log(doc.id);
-        const data = doc.data();
-        // ////console.log(data);
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
+  const products = collection(firebase.db, 'products');
+  const docSnap = await getDocs(products);
+  // ////console.log(docSnap.docs);
+  const table = document.getElementById('uploads-table');
+  // ////console.log(creatorsTable);
+  docSnap.docs.reverse().forEach((doc) => {
+    ////console.log(doc.id);
+    const data = doc.data();
+    // ////console.log(data);
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
               <td><img src='${data.image}' width='70px' height='70px'/></td>
               <td>${data.email}</td>
               <td><button style="background-color: #ff69b4; padding: 10px; border-radius: 25px; border: none; color: white;font-weight: 4500;cursor: pointer; cursor: pointer;" onclick="window.open('${data.mainFile}')">Download</button></td>
@@ -21,22 +24,26 @@ export async function setProducts() {
               <button style="background-color: #a9203e; padding: 10px; border-radius: 25px; border: none; color: white;font-weight: 4500; cursor: pointer;" id='${doc.id}' class='removeProduct' style="cursor: pointer;">Remove</button>
                 </td>
             `;
-        table.appendChild(tr);
-    });
+    table.appendChild(tr);
+  });
 }
-{/* <button id='${doc.id}' class='viewProfile' style="cursor: pointer;">View</button> */}
-
+{
+  /* <button id='${doc.id}' class='viewProfile' style="cursor: pointer;">View</button> */
+}
 
 export async function setCreators() {
-    const creators = collection(firebase.db, "joinedus");
-    const docSnap = await getDocs(creators);
-    const creatorsTable = document.getElementById("creators-table");
-    // ////console.log(creatorsTable);
-    docSnap.docs.reverse().slice(0, 5).forEach((doc) => {
-        // ////console.log(doc.data());
-        const data = doc.data();
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
+  const creators = collection(firebase.db, 'joinedus');
+  const docSnap = await getDocs(creators);
+  const creatorsTable = document.getElementById('creators-table');
+  // ////console.log(creatorsTable);
+  docSnap.docs
+    .reverse()
+    .slice(0, 5)
+    .forEach((doc) => {
+      // ////console.log(doc.data());
+      const data = doc.data();
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
               <td><a href="./creator.html?id=${doc.id}">${data.fullName}</a></td>
               <td>${data.email}</td>
               <td>${data.contact}</td>
@@ -45,22 +52,22 @@ export async function setCreators() {
               <button style="background-color: #ff69b4; padding: 10px; border-radius: 25px; border: none; color: white;font-weight: 4500; cursor: pointer;" id='${doc.id}' class='viewProfile' style="cursor: pointer;">View</button>
                 </td>
             `;
-        creatorsTable.appendChild(tr);
+      creatorsTable.appendChild(tr);
     });
 }
 
 export async function setAllCreators() {
-    const creators = collection(firebase.db, "joinedus");
-    const docSnap = await getDocs(creators);
-    const creatorsTable = document.getElementById("creators-table-creators");
-    // ////console.log(creatorsTable);
-    docSnap.docs.reverse().forEach((doc) => {
-        // ////console.log(doc.data());
-        const data = doc.data();
-        // ////console.log(data, doc.id)
-        const tr = document.createElement("tr");
-        let id = doc.id;
-        tr.innerHTML = `
+  const creators = collection(firebase.db, 'joinedus');
+  const docSnap = await getDocs(creators);
+  const creatorsTable = document.getElementById('creators-table-creators');
+  // ////console.log(creatorsTable);
+  docSnap.docs.reverse().forEach((doc) => {
+    // ////console.log(doc.data());
+    const data = doc.data();
+    // ////console.log(data, doc.id)
+    const tr = document.createElement('tr');
+    let id = doc.id;
+    tr.innerHTML = `
               <td><a href="./creator.html?id=${doc.id}">${data.fullName}</a></td>
               <td>${data.email}</td>
               <td>${data.contact}</td>
@@ -69,41 +76,41 @@ export async function setAllCreators() {
               <button style="background-color: #ff69b4; padding: 10px; border-radius: 25px; border: none; color: white;font-weight: 4500; cursor: pointer;" id='${doc.id}' class='viewProfile' >View</button>
               </td>
             `;
-        creatorsTable.appendChild(tr);
-    });
+    creatorsTable.appendChild(tr);
+  });
 }
 
 export async function fetchIncome() {
-    const creators = collection(firebase.db, 'joinedus');
-    const docs = await getDocs(creators);
-    const incomeTable = document.getElementById('income-table');
-    let overallIncome = 0;
-    docs.docs.forEach((doc) => {
-        const data = doc.data();
+  const creators = collection(firebase.db, 'joinedus');
+  const docs = await getDocs(creators);
+  const incomeTable = document.getElementById('income-table');
+  let overallIncome = 0;
+  docs.docs.forEach((doc) => {
+    const data = doc.data();
 
-        const name = data.fullName;
-        let totalIncome = 0;
-        data.plansSold.map((e) => totalIncome += parseFloat(e.price))
-        ////console.log(totalIncome)
-        const tr = document.createElement("tr");
-        
-        let oneMonthIncome = 0;
-        var seconds = new Date().getTime() / 1000;
-        const todayTS = seconds * 1000;
-        const past30thTS = (seconds  -2592000)*1000;
-        overallIncome += totalIncome;
-        data.plansSold.map((e) => {
-                ////console.log(e.timestamp.seconds * 1000) //Timestamp Of The Date When The Product Was Purchased
-                ////console.log(seconds * 1000) //Today's Timestamp
-                ////console.log((seconds  -2592000)*1000) //Timestamp Of Past 30th Day From Today
-                
-                const purchaseTS = e.timestamp.seconds * 1000; 
+    const name = data.fullName;
+    let totalIncome = 0;
+    data.plansSold.map((e) => (totalIncome += parseFloat(e.price)));
+    ////console.log(totalIncome)
+    const tr = document.createElement('tr');
 
-                if(todayTS - purchaseTS <  todayTS - past30thTS){
-                    oneMonthIncome+=parseFloat(e.price)
-                }
-        })
-        tr.innerHTML = `
+    let oneMonthIncome = 0;
+    var seconds = new Date().getTime() / 1000;
+    const todayTS = seconds * 1000;
+    const past30thTS = (seconds - 2592000) * 1000;
+    overallIncome += totalIncome;
+    data.plansSold.map((e) => {
+      ////console.log(e.timestamp.seconds * 1000) //Timestamp Of The Date When The Product Was Purchased
+      ////console.log(seconds * 1000) //Today's Timestamp
+      ////console.log((seconds  -2592000)*1000) //Timestamp Of Past 30th Day From Today
+
+      const purchaseTS = e.timestamp.seconds * 1000;
+
+      if (todayTS - purchaseTS < todayTS - past30thTS) {
+        oneMonthIncome += parseFloat(e.price);
+      }
+    });
+    tr.innerHTML = `
               <td><a href="./creator.html?id=${doc.id}">${data.fullName}</a></td>
               <td>${data.email}</td>
               <td>${oneMonthIncome}</td>
@@ -111,32 +118,32 @@ export async function fetchIncome() {
               <td>
               </td>
             `;
-        incomeTable.appendChild(tr);
-    })
+    incomeTable.appendChild(tr);
+  });
 
-    const inc = document.getElementById('income')
-    inc.textContent = overallIncome + ' INR';
-    // inc.innerText({oneMonthIncome}))
+  const inc = document.getElementById('income');
+  inc.textContent = overallIncome + ' INR';
+  // inc.innerText({oneMonthIncome}))
 }
 
 export const fetchAllStats = async () => {
-     const creators = collection(firebase.db, 'joinedus');
-     const products = collection(firebase.db, 'products');
-     const income = collection(firebase.db, 'joinedus');
-     const creatorDocSnap = await getDocs(creators);
-     const productDocSnap = await getDocs(products);
-     const incomeDocSnap = await getDocs(income);
-     const creatorsTable = document.getElementById('stats');
-     // ////console.log(creatorsTable);
-     let overallIncome = 0;
-    incomeDocSnap.docs.forEach((doc) => {
-      const data = doc.data();
-      console.log(data.plansSold)
-      let totalIncome = 0;
-      data.plansSold.map((e) => (totalIncome += parseFloat(e.price)));
-      overallIncome += totalIncome;
-    });
-     creatorsTable.innerHTML = `
+  const creators = collection(firebase.db, 'joinedus');
+  const products = collection(firebase.db, 'products');
+  const income = collection(firebase.db, 'joinedus');
+  const creatorDocSnap = await getDocs(creators);
+  const productDocSnap = await getDocs(products);
+  const incomeDocSnap = await getDocs(income);
+  const creatorsTable = document.getElementById('stats');
+  // ////console.log(creatorsTable);
+  let overallIncome = 0;
+  incomeDocSnap.docs.forEach((doc) => {
+    const data = doc.data();
+    console.log(data.plansSold);
+    let totalIncome = 0;
+    data.plansSold.map((e) => (totalIncome += parseFloat(e.price)));
+    overallIncome += totalIncome;
+  });
+  creatorsTable.innerHTML = `
       <div class="main-content-info container">
         <div style="cursor: pointer" onclick="window.location = '/creators.html'" class="mini-card">
           <h2 class="cus-num cus-h"></h2>
@@ -155,21 +162,21 @@ export const fetchAllStats = async () => {
         </div>
         <div class="clear"></div>
       </div>`;
-    //  docSnap.docs.reverse().forEach((doc) => {
-    //    // ////console.log(doc.data());
-    //    const data = doc.data();
-    //    // ////console.log(data, doc.id)
-    //    const tr = document.createElement('tr');
-    //    let id = doc.id;
-    //    tr.innerHTML = `;
-    //           <td><a href="./creator.html?id=${doc.id}">${data.fullName}</a></td>
-    //           <td>${data.email}</td>
-    //           <td>${data.contact}</td>
-    //           <td>
-    //           <button id='${doc.id}' class='removeButton'  style="cursor: pointer;">Remove</button>
-    //           <button id='${doc.id}' class='viewProfile' style="cursor: pointer;">View</button>
-    //           </td>
-    //         `;
-    //    creatorsTable.appendChild(tr);
-    //  });
-}
+  //  docSnap.docs.reverse().forEach((doc) => {
+  //    // ////console.log(doc.data());
+  //    const data = doc.data();
+  //    // ////console.log(data, doc.id)
+  //    const tr = document.createElement('tr');
+  //    let id = doc.id;
+  //    tr.innerHTML = `;
+  //           <td><a href="./creator.html?id=${doc.id}">${data.fullName}</a></td>
+  //           <td>${data.email}</td>
+  //           <td>${data.contact}</td>
+  //           <td>
+  //           <button id='${doc.id}' class='removeButton'  style="cursor: pointer;">Remove</button>
+  //           <button id='${doc.id}' class='viewProfile' style="cursor: pointer;">View</button>
+  //           </td>
+  //         `;
+  //    creatorsTable.appendChild(tr);
+  //  });
+};
